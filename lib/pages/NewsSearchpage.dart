ff28a64ec1efe9a9294030461a14bddaaff8f331
easycodingcourse <easycodingcourse@gmail.com>
2022-02-26 16:11:11 +0600

    search bar add

--- lib/pages/NewsSearchpage.dart
+++ lib/pages/NewsSearchpage.dart
@@ -10,6 +10,54 @@ class NewsSearch extends StatefulWidget {
 class _NewsSearchState extends State<NewsSearch> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.white,
+        title: Text("Search",style: TextStyle(color: Colors.black),),
+        centerTitle: true,
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(kToolbarHeight),
+          child: Container(
+            //  padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.grey[300],
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: Material(
+                  color: Colors.grey[300],
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Icon(Icons.search,color: Colors.grey),
+                      Expanded(
+                        child: TextField(
+                          // textAlign: TextAlign.center,
+                          decoration: InputDecoration.collapsed(
+                            hintText: ' Search by name or address',
+                          ),
+                          onChanged: (value) {
+
+                          },
+                        ),
+                      ),
+                      InkWell(
+                        child: Icon(Icons.mic,color: Colors.grey,),
+                        onTap: () {
+
+                        },
+                      )
+                    ],
+                  ),
+                ),
+              )
+          ) ,
+        ),
+
+      ),
+
+    );
   }
 }
